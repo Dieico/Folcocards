9a4be61c434b83cba82805e8170462b6e40b0ede
Dieico <diegolimd3@gmail.com>
2020-02-25 23:53:04 -0300

    v3

---
+++ precache-manifest.aa6725ae38ebdc94af697e583b4616ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b170b72a40a98b6c892bda2f70908d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e6df04c9f29181edd0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "57ddf22e9bd897bfd334",
+    "url": "/static/css/main.f5690774.chunk.css"
+  },
+  {
+    "revision": "75e6df04c9f29181edd0",
+    "url": "/static/js/2.bd245d60.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.bd245d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ddf22e9bd897bfd334",
+    "url": "/static/js/main.2b3a84ab.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
